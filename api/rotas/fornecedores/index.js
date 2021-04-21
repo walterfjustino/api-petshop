1885eb4463abf452918b506358988a9973ece3be
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-04-21 00:06:45 -0300

    Created Post route, add nodemon dependency

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,7 +1,8 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
-roteador.use('/', async (requisicao, resposta) => {
+roteador.get('/', async (requisicao, resposta) => {
     try {
         const resultados = await TabelaFornecedor.listar()
     resposta.send(
@@ -13,6 +14,17 @@ roteador.use('/', async (requisicao, resposta) => {
             })
         )
     }
+    
+})
+
+roteador.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    resposta.send(
+        JSON.stringify(fornecedor)
+    )
+
 })
 
 module.exports = roteador
