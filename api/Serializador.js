682e1d9d800dc5901fa4c83d4b8adfb68f2ba9e2
Walter Fernandes Justino <walterfjustino@gmail.com>
2021-04-23 17:36:11 -0300

    Created Serializer that checks the content received in the api request

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,20 @@
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado")
 
-
+/*TRANSFORMA OS DADOS RECEBIDOS EM JSON */
 class Serializador {
     json (dados) {
         return JSON.stringify(dados)
     }
-
+/*RECEBE OS DADOS, VERIFICA SE O CONTEUDO É JSON, RETORNA JSON, SE NÃO FOR JSON, RETORNA A EXCECÃO VALOR NÃO SUPORTADO */
     serializar (dados) {
         if (this.contentType === 'application/json') {
             return this.json(dados)
         }
         throw new ValorNaoSuportado(this.contentType)
     }
+}
+
+module.exports = {
+    Serializador: Serializador,
+    formatosAceitos: ['application/json']
 }
